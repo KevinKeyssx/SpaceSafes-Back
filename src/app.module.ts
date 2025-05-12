53fa832c23e0bc69a44f977a48cb156b74cedbfb
kevinkeyssx <keyss1700@gmail.com>
2025-05-12 01:40:22 -0400

    App module added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { AppController }    from './app.controller';
+import { AppService }       from './app.service';
+import { AccountsModule }   from '@accounts/accounts.module';
+import { NavlyModule }      from '@navly/navly.module';
+import { NotesModule }      from '@notes/notes.module';
+import { BalancesModule }   from '@balances/balances.module';
+import { PaymentsModule }   from '@payments/payments.module';
+import { CommonModule }     from '@common/common.module';
 
 @Module({
-  imports: [],
+  imports: [AccountsModule, NavlyModule, NotesModule, BalancesModule, PaymentsModule, CommonModule],
   controllers: [AppController],
   providers: [AppService],
 })
