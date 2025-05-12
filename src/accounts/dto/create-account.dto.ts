1c97d673553298244b011b91e70cc7f36abc6b33
kevinkeyssx <keyss1700@gmail.com>
2025-05-12 01:43:10 -0400

    Account DTO added

---
+++ src/accounts/dto/create-account.dto.ts
@@ -0,0 +1,6 @@
+export class CreateAccountDto {
+    username    : string;
+    password    : string;
+    userId      : string;
+    url?        : string;
+}
---
+++ src/accounts/entities/account.entity.ts
@@ -0,0 +1,9 @@
+export class AccountEntity {
+    id: string;
+    userId: string;
+    username: string;
+    password: string;
+    createdAt: Date;
+    updatedAt: Date;
+    // navlys: Navly[];
+}
