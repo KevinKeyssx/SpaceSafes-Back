f1aa430855c14d65496ba20e323ae0a351acf56b
kevinkeyssx <keyss1700@gmail.com>
2025-05-13 01:54:48 -0400

    Navly services updated

--- src/navly/navly.service.ts
+++ src/navly/navly.service.ts
@@ -1,26 +1,93 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+
+import { PrismaClient } from '@prisma/client';
+import ogs              from 'open-graph-scraper';
+
 import { CreateNavlyDto } from './dto/create-navly.dto';
 import { UpdateNavlyDto } from './dto/update-navly.dto';
+import { PrismaException } from '@common/error/prisma-catch';
+
 
 @Injectable()
-export class NavlyService {
-  create(createNavlyDto: CreateNavlyDto) {
-    return 'This action adds a new navly';
-  }
+export class NavlyService extends PrismaClient implements OnModuleInit {
+    onModuleInit() {
+		this.$connect();
+	}
 
-  findAll() {
-    return `This action returns all navly`;
-  }
+    async create( createNavlyDto: CreateNavlyDto ) {
+        try {
+            const { result }    = await ogs({ url: createNavlyDto.url });
+            const avatar        = result.ogImage?.[0].url;
 
-  findOne(id: number) {
-    return `This action returns a #${id} navly`;
-  }
+            createNavlyDto.description  ??= result.ogDescription;
+            createNavlyDto.name         ??= result.ogTitle;
 
-  update(id: number, updateNavlyDto: UpdateNavlyDto) {
-    return `This action updates a #${id} navly`;
-  }
+            const navly = await this.navly.create({
+                data: {
+                    ...createNavlyDto,
+                    avatar
+                },
+            });
 
-  remove(id: number) {
-    return `This action removes a #${id} navly`;
-  }
+            return navly;
+        } catch ( error ) {
+            throw PrismaException.catch( error, 'Navly' );
+        }
+    }
+
+    async findAll(
+        userId: string
+    ) {
+        try {
+            return await this.navly.findMany({
+                where: { userId },
+            });
+        } catch ( error ) {
+            throw PrismaException.catch( error, 'Navly' );
+        }
+    }
+
+    async findOne(
+        id: string
+    ) {
+        try {
+            return await this.navly.findUnique({
+                where: { id },
+            });
+        } catch ( error ) {
+            throw PrismaException.catch( error, 'Navly' );
+        }
+    }
+
+    async update(
+        id: string,
+        updateNavlyDto: UpdateNavlyDto
+    ) {
+        if ( updateNavlyDto.url ) {
+            const { result }    = await ogs({ url: updateNavlyDto.url });
+
+            updateNavlyDto.description  ??= result.ogDescription;
+            updateNavlyDto.name         ??= result.ogTitle;
+            updateNavlyDto.avatar       ??= result.ogImage?.[0].url;
+        }
+
+        try {
+            return await this.navly.update({
+                where: { id },
+                data: updateNavlyDto,
+            });
+        } catch ( error ) {
+            throw PrismaException.catch( error, 'Navly' );
+        }
+    }
+
+    async remove( id: string ) {
+        try {
+            return await this.navly.delete({
+                where: { id },
+            });
+        } catch ( error ) {
+            throw PrismaException.catch( error, 'Navly' );
+        }
+    }
 }
