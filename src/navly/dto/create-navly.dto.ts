f79685e18ae3c16e527e3ac6bb439252e764e0d3
kevinkeyssx <keyss1700@gmail.com>
2025-05-13 01:55:17 -0400

    Create navly dto updated

--- src/navly/dto/create-navly.dto.ts
+++ src/navly/dto/create-navly.dto.ts
@@ -1,1 +1,10 @@
-export class CreateNavlyDto {}
+import { IntersectionType } from "@nestjs/swagger";
+
+import { BasicNavlyDto }    from "@navly/dto/basic-navly.dto";
+import { UserIdDto }        from "@common/dtos/user-id.dto";
+
+
+export class CreateNavlyDto extends IntersectionType(
+    BasicNavlyDto,
+    UserIdDto
+) {}
