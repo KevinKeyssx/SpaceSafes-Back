38b25bf4edd2bf2036b794fccaf97cda783fd683
kevinkeyssx <keyss1700@gmail.com>
2025-05-13 01:51:18 -0400

    Balances controller updated

--- src/balances/balances.controller.ts
+++ src/balances/balances.controller.ts
@@ -1,34 +1,47 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe } from '@nestjs/common';
 import { BalancesService } from './balances.service';
 import { CreateBalanceDto } from './dto/create-balance.dto';
 import { UpdateBalanceDto } from './dto/update-balance.dto';
 
 @Controller('balances')
 export class BalancesController {
-  constructor(private readonly balancesService: BalancesService) {}
+    constructor(private readonly balancesService: BalancesService) {}
 
-  @Post()
-  create(@Body() createBalanceDto: CreateBalanceDto) {
-    return this.balancesService.create(createBalanceDto);
-  }
+    @Post()
+    create(
+        @Body() createBalanceDto: CreateBalanceDto
+    ) {
+        return this.balancesService.create( createBalanceDto );
+    }
 
-  @Get()
-  findAll() {
-    return this.balancesService.findAll();
-  }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.balancesService.findOne(+id);
-  }
+    @Get('users/:id')
+    findAll(
+        @Param('id') userId: string
+    ) {
+        return this.balancesService.findAll( userId );
+    }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBalanceDto: UpdateBalanceDto) {
-    return this.balancesService.update(+id, updateBalanceDto);
-  }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.balancesService.remove(+id);
-  }
+    @Get(':id')
+    findOne(@Param('id', ParseUUIDPipe ) id: string) {
+        return this.balancesService.findOne( id );
+    }
+
+
+    @Patch(':id')
+    update(
+        @Param('id', ParseUUIDPipe ) id: string,
+        @Body() updateBalanceDto: UpdateBalanceDto
+    ) {
+        return this.balancesService.update( id, updateBalanceDto );
+    }
+
+
+    @Delete(':id')
+    remove(
+        @Param('id', ParseUUIDPipe ) id: string
+    ) {
+        return this.balancesService.remove( id );
+    }
 }
