4bfee4076029fb4c337d0d86809ad4d69dabe6cf
kevinkeyssx <keyss1700@gmail.com>
2025-05-13 01:52:54 -0400

    Balance enum added

---
+++ src/balances/enums/balance.enum.ts
@@ -0,0 +1,28 @@
+export enum TypeBalance {
+    CREDIT          = 'CREDIT',
+    DEBIT           = 'DEBIT',
+    BANK_ACCOUNT    = 'BANK_ACCOUNT',
+    CASH            = 'CASH',
+    FREELANCE       = 'FREELANCE',
+    SAVINGS         = 'SAVINGS',
+    INVESTMENT      = 'INVESTMENT',
+    OTHER           = 'OTHER'
+}
+
+export enum TypeCard {
+    VISA                = 'VISA',
+    VISA_ELECTRON       = 'VISA_ELECTRON',
+    MASTERCARD          = 'MASTERCARD',
+    AMERICAN_EXPRESS    = 'AMERICAN_EXPRESS',
+    DISCOVER            = 'DISCOVER',
+    JCB                 = 'JCB',
+    DINERS_CLUB         = 'DINERS_CLUB',
+    UNIONPAY            = 'UNIONPAY',
+    MAESTRO             = 'MAESTRO',
+    ELO                 = 'ELO',
+    HIPERCARD           = 'HIPERCARD',
+    AURA                = 'AURA'
+}
+
+export const TypeBalanceValues = Object.values(TypeBalance);
+export const TypeCardValues = Object.values(TypeCard);
