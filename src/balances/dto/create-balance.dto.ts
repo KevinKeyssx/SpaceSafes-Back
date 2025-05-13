d9306001fcb064a19f2345ef02a9e02a55bbf75e
kevinkeyssx <keyss1700@gmail.com>
2025-05-13 01:52:03 -0400

    Create balance dto updated

--- src/balances/dto/create-balance.dto.ts
+++ src/balances/dto/create-balance.dto.ts
@@ -1,1 +1,10 @@
-export class CreateBalanceDto {}
+import { IntersectionType } from '@nestjs/swagger';
+
+import { UserIdDto }        from '@common/dtos/user-id.dto';
+import { BasicBalanceDto }  from '@balances/dto/basic-balance.dto';
+
+
+export class CreateBalanceDto  extends IntersectionType(
+    BasicBalanceDto,
+    UserIdDto
+) {}
