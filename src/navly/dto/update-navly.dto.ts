e15f219e08d7222cc8a1ed8cec9f21b67a68b0e7
kevinkeyssx <keyss1700@gmail.com>
2025-05-13 01:55:32 -0400

    Update navly dto avatar added

--- src/navly/dto/update-navly.dto.ts
+++ src/navly/dto/update-navly.dto.ts
@@ -1,4 +1,16 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateNavlyDto } from './create-navly.dto';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 
-export class UpdateNavlyDto extends PartialType(CreateNavlyDto) {}
+import { BasicNavlyDto } from './basic-navly.dto';
+import { IsOptional, IsString, Length } from 'class-validator';
+
+export class UpdateNavlyDto extends PartialType( BasicNavlyDto ) {
+    @ApiProperty({
+        description: 'Avatar associated with the navly',
+        example    : 'https://example.com/navly',
+        required   : false
+    })
+    @IsOptional()
+    @IsString()
+    @Length( 1, 100 )
+    avatar?: string;
+}
