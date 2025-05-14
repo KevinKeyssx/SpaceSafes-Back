aa8dfc4006787a0b2f5ba4f71bf673f75aaf2ced
kevinkeyssx <keyss1700@gmail.com>
2025-05-14 05:52:42 -0400

    BalancePayment DTO updated

--- src/navly/dto/create-navly.dto.ts
+++ src/navly/dto/create-navly.dto.ts
@@ -1,10 +1,12 @@
 import { IntersectionType } from "@nestjs/swagger";
 
-import { BasicNavlyDto }    from "@navly/dto/basic-navly.dto";
-import { UserIdDto }        from "@common/dtos/user-id.dto";
+import { BasicNavlyDto }        from "@navly/dto/basic-navly.dto";
+import { UserIdDto }            from "@common/dtos/user-id.dto";
+import { BalancePaymentDto }    from "@common/dtos/balance-payment.dto";
 
 
 export class CreateNavlyDto extends IntersectionType(
     BasicNavlyDto,
-    UserIdDto
+    UserIdDto,
+    BalancePaymentDto
 ) {}
