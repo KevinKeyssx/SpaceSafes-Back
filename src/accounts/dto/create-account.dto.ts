2ca45f9ac97dedca96726ff80f67ab9a6f0268d4
kevinkeyssx <keyss1700@gmail.com>
2025-05-14 05:47:40 -0400

    BalancePayment DTO updated

--- src/accounts/dto/create-account.dto.ts
+++ src/accounts/dto/create-account.dto.ts
@@ -1,11 +1,13 @@
 import { IntersectionType } from '@nestjs/swagger';
 
-import { BasicAccountDto } from './basic-account.dto';
-import { UserIdDto } from '@common/dtos/user-id.dto';
+import { BasicAccountDto }      from './basic-account.dto';
+import { UserIdDto }            from '@common/dtos/user-id.dto';
+import { BalancePaymentDto }    from '@common/dtos/balance-payment.dto';
 
 
 export class CreateAccountDto extends IntersectionType(
     BasicAccountDto,
-    UserIdDto
+    UserIdDto,
+    BalancePaymentDto
 ) {}
 
