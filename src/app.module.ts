9897b2da029b793ac9f6cd3fcb95718f27e5ca9d
kevinkeyssx <keyss1700@gmail.com>
2025-05-14 23:22:36 -0400

    Formatted and sorted

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,29 @@
 import { Module } from '@nestjs/common';
 
 import { AppController }            from './app.controller';
-import { AppService }               from './app.service';
 import { AccountsModule }           from '@accounts/accounts.module';
 import { NavlyModule }              from '@navly/navly.module';
 import { NotesModule }              from '@notes/notes.module';
 import { BalancesModule }           from '@balances/balances.module';
-import { PaymentsModule }           from '@payments/payments.module';
 import { CommonModule }             from '@common/common.module';
 import { ServicesModule }           from '@services/services.module';
 import { ExpensesModule }           from '@expenses/expenses.module';
 import { PaymentServicesModule }    from '@payment-services/payment-services.module';
+import { PaymentDetailsModule }     from '@payment-details/payment-details.module';
 
 @Module({
-    imports: [AccountsModule, NavlyModule, NotesModule, BalancesModule, PaymentsModule, CommonModule, ServicesModule, ExpensesModule, PaymentServicesModule],
-    controllers: [AppController],
-    providers: [AppService],
+    imports     : [
+        AccountsModule,
+        NavlyModule,
+        NotesModule,
+        BalancesModule,
+        CommonModule,
+        ServicesModule,
+        ExpensesModule,
+        PaymentServicesModule,
+        PaymentDetailsModule
+    ],
+    controllers : [AppController],
+    providers   : [],
 })
 export class AppModule {}
