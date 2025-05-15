0663529ec1aca414c39a51baf34c202330c43fb0
kevinkeyssx <keyss1700@gmail.com>
2025-05-14 23:23:04 -0400

    Space removed

--- src/balances/dto/create-balance.dto.ts
+++ src/balances/dto/create-balance.dto.ts
@@ -4,7 +4,7 @@ import { UserIdDto }        from '@common/dtos/user-id.dto';
 import { BasicBalanceDto }  from '@balances/dto/basic-balance.dto';
 
 
-export class CreateBalanceDto  extends IntersectionType(
+export class CreateBalanceDto extends IntersectionType(
     BasicBalanceDto,
     UserIdDto
 ) {}
