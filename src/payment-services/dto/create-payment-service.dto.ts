40a5b46243e8123e7cf02e83820a3d5a97a10033
kevinkeyssx <keyss1700@gmail.com>
2025-05-14 23:26:49 -0400

    Create Payment service dto updated

--- src/payment-services/dto/create-payment-service.dto.ts
+++ src/payment-services/dto/create-payment-service.dto.ts
@@ -1,1 +1,9 @@
-export class CreatePaymentServiceDto {}
+import { IntersectionType } from "@nestjs/swagger";
+
+import { BasicPaymentServiceDto } from "./basic-payment-service.dto";
+import { UserIdDto } from "@common/dtos/user-id.dto";
+
+export class CreatePaymentServiceDto extends IntersectionType(
+    BasicPaymentServiceDto,
+    UserIdDto,
+) {}
