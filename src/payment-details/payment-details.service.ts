ca037ac2f7f2cd06b2aa0634de54baa61a56b167
kevinkeyssx <keyss1700@gmail.com>
2025-05-19 04:09:14 -0400

    The amount on the balance is updated each time a payment is made & filter in findAll updated

--- src/payment-details/payment-details.service.ts
+++ src/payment-details/payment-details.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, NotFoundException, OnModuleInit } from '@nestjs/common';
 
 import { PrismaClient, StatusPayment } from '@prisma/client';
 
@@ -11,6 +11,7 @@ export class PaymentDetailsService extends PrismaClient implements OnModuleInit 
     onModuleInit() {
 		this.$connect();
 	}
+
 
     async createMultiple( createMultiplePaymentDetailDto: CreateMultiplePaymentDetailDto ) {
         try {
@@ -49,12 +50,39 @@ export class PaymentDetailsService extends PrismaClient implements OnModuleInit 
                 status: StatusPayment.COMPLETED
             }));
 
-            const multiplePaymentDetailsDB = await this.paymentDetail.createMany({
+            await this.paymentDetail.createMany({
                 data: multiplePaymentDetails,
             });
 
-            return multiplePaymentDetailsDB;
-        } catch (error) {
+            const balanceReduce: { ids: string; amount: number }[] = paymentDetails
+            .reduce((acc, paymentDetail) => {
+                const existingItem = acc.find(item => item.ids === paymentDetail.balanceId);
+
+                if (existingItem) {
+                    existingItem.amount += paymentDetail.amount;
+                } else {
+                    acc.push({ ids: paymentDetail.balanceId, amount: paymentDetail.amount });
+                }
+
+                return acc;
+            }, [] as { ids: string; amount: number }[]);
+
+            await this.$transaction(async (tx) => {
+                for (const reduction of balanceReduce) {
+                    await tx.balance.update({
+                        where: { id: reduction.ids },
+                        data: {
+                            lastPayment: new Date(),
+                            balance: {
+                                decrement: reduction.amount,
+                            },
+                        },
+                    });
+                }
+            });
+
+            return this.findAll( userId, month, year );
+        } catch ( error ) {
             throw PrismaException.catch( error, 'PaymentDetail' );
         }
     }
@@ -62,14 +90,86 @@ export class PaymentDetailsService extends PrismaClient implements OnModuleInit 
 
     async findAll( userId: string, month: number, year: number ) {
         return await this.paymentDetail.findMany({
+            select: {
+                id: true,
+                amount: true,
+                status: true,
+                createdAt: true,
+                paymentService: {
+                    select: {
+                        id: true,
+                        description: true
+                    }
+                },
+                balance: {
+                    select: {
+                        id : true,
+                        name: true,
+                        type: true,
+                        balance: true,
+                        lastPayment: true,
+                    }
+                },
+                payment: {
+                    select: {
+                        id: true,
+                        month: true,
+                        year: true
+                    }
+                }
+            },
             where: { userId, payment: { month, year } },
         });
     }
 
-    async remove( userId: string, ids: string[] ) {
+
+    async removeMultiple( userId: string, ids: string[] ) {
         try {
+            const paymentDetails = await this.paymentDetail.findMany({
+                where: { userId, id: { in: ids } },
+            });
+
+            await this.$transaction(async (tx) => {
+                for (const paymentDetail of paymentDetails) {
+                    await tx.balance.update({
+                        where   : { id: paymentDetail.balanceId },
+                        data    : {
+                            lastPayment: new Date(),
+                            balance: {
+                                increment: paymentDetail.amount,
+                            },
+                        },
+                    });
+                }
+            });
+
             return await this.paymentDetail.deleteMany({
                 where: { userId, id: { in: ids } },
+            });
+        } catch ( error ) {
+            throw PrismaException.catch( error, 'PaymentDetail' );
+        }
+    }
+
+
+    async remove( id: string ) {
+        try {
+            const paymentDetail = await this.paymentDetail.findUnique({
+                where: { id },
+            });
+
+            await this.balance.update({
+                where   : { id: paymentDetail!.balanceId },
+                data    : {
+                    lastPayment: new Date(),
+                    balance: {
+                        increment: paymentDetail!.amount,
+                    },
+                },
+            });
+
+            return await this.paymentDetail.delete({
+                where: { id },
             });
         } catch ( error ) {
             throw PrismaException.catch( error, 'PaymentDetail' );
