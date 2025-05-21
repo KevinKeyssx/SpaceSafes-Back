caf1563bd1394266bbfca8b08faeba1bd5b97d37
kevinkeyssx <keyss1700@gmail.com>
2025-05-20 22:02:03 -0400

    Impor navly service

--- src/accounts/accounts.module.ts
+++ src/accounts/accounts.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AccountsService } from './accounts.service';
 import { AccountsController } from './accounts.controller';
+import { NavlyModule } from '@navly/navly.module';
 
 @Module({
+  imports: [NavlyModule],
   controllers: [AccountsController],
   providers: [AccountsService],
+  exports: [AccountsService]
 })
 export class AccountsModule {}
